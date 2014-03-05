1cd37d59942e9fe454cddddc5c1ea56b8bfe2c9e
lucasgehin <lucasgehin88@gmail.com>
2014-03-05 22:11:44 +0100

    design du chat (responsive ...)

--- public/javascripts/chat/main.js
+++ public/javascripts/chat/main.js
@@ -5,7 +5,7 @@ var resize_chat, $load_img, load_count, load_start, load_end;
 resize_chat = function () {
     "use strict";
     var height_chat;
-    height_chat = $(window).height() - $('.navbar').height() * 3.45;
+    height_chat = $(window).height() - $('.navbar').height() * 1.45;
     $("#chat").height(height_chat);
 };
 
